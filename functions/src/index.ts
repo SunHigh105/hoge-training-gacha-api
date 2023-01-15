92a2d071b31692c3d393655bb94426d7e307e65e
Ayaka Hidaka <hidaka.ayaka105@gmail.com>
2023-01-15 21:41:20 +0900

    Create training menu api

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -13,14 +13,54 @@ app.use(cors({
 }));
 
 const firestore = require('./firestore');
+const createMenu = require('./createMenu');
+const constants = require('./constants');
 
 app.get('/', (res: any) => {
   res.send('Simple REST API');
 });
 
-app.get('/menus', async (req: any, res: any) => {
-  const response = await firestore.getData();
-  res.json(response);
+app.get('/training_menus', async (req: any, res: any) => {
+  try {
+    // Request validation
+    // TODO: リクエストパラメータmuscle,cardioのバリデーション
+    if (!req.query.minute) {
+      throw new Error('Request parameter "minute" required.');
+    }
+    if (!constants.MINUTE_PATTERN[req.query.minute]) {
+      throw new Error(`Request parameter "minute" is invalid value. Valid values: ${Object.keys(constants.MINUTE_PATTERN).join(', ')}`);
+    }
+    
+    const muscleCategories = req.query.muscle
+      ? createMenu.getFilterdCategories(constants.CATEGORY_MUSCLE_TRAININGS, req.query.muscle)
+      : Object.values(constants.CATEGORY_MUSCLE_TRAININGS);
+    const cardioCategories = req.query.cardio
+      ? createMenu.getFilterdCategories(constants.CATEGORY_CARDIO_HIIT, req.query.cardio)
+      : Object.values(constants.CATEGORY_CARDIO_HIIT);
+    
+    const minutePattern = createMenu.getMinutePattern(constants.MINUTE_PATTERN[req.query.minute]);
+
+    const trainingList = await firestore.getData([...muscleCategories, ...cardioCategories]);
+
+    const muscleTrainings = createMenu.createTrainingMenu(
+      trainingList,
+      minutePattern.muscle,
+      muscleCategories
+    );
+    const cardioHiits = createMenu.createTrainingMenu(
+      trainingList,
+      minutePattern.cardio,
+      cardioCategories
+    );
+
+    res.json([...muscleTrainings, ...cardioHiits]);
+  } catch (e) {
+    if (e instanceof Error) {
+      res.json({"Create training menu failed.": e.message});
+      return;
+    }
+    res.json(["Unexpected error."]);
+  }
 });
 
 exports.app = functions
