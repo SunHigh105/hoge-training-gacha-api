2b2153360605e97254ed01d31b973b10bf53a544
Ayaka Hidaka <hidaka.ayaka105@gmail.com>
2023-01-15 21:40:18 +0900

    Modify create training menu test

--- functions/src/tests/createMenu.test.ts
+++ functions/src/tests/createMenu.test.ts
@@ -2,53 +2,20 @@ const createMenu = require('../createMenu');
 const trainingList = require('./json/trainingList.json');
 
 describe('createTrainingMenu', () => {
-  test('Training & Cardio,Hiit | No category specified', () => {
-    const pattern = {
-      training: [5, 5, 5]
-    }
-    const filterCondition = {
-      channelId: [],
-      training: [],
-      cardioHiit: [],
-    }
-    const menu = createMenu.createTrainingMenu(trainingList, pattern, filterCondition);
+  test('Category specified', () => {
+    const pattern = [5, 5, 5];
+    const categories = ['腹筋', '背中']
+    const menu = createMenu.createTrainingMenu(trainingList, pattern, categories);
     console.log(menu);
     expect(true);
   });
 
-  test('Training only | No category specified', () => {
-    // TODO
-  });
-
-  test('Cardio,Hiit only | No category specified', () => {
-    // TODO
-  });
-
-  test('Training & Cardio,Hiit | category specified', () => {
+  test('No category specified', () => {
     // TODO
   });
 });
 
 describe('getFilterdCategories', () => {
-  test('No filtered condition', () => {
-    const result = createMenu.getFilterdCategories(
-      {
-        abs: '腹筋',
-        leg: '脚',
-        hip: '尻',
-        back: '背中',
-      },
-      []
-    );
-
-    expect(result).toEqual([
-      '腹筋',
-      '脚',
-      '尻',
-      '背中'
-    ]);
-  });
-
   test('One filtered condition exists', () => {
     const result = createMenu.getFilterdCategories(
       {
