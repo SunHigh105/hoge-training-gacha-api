9e437c358a3d071d183b3ab5263cc358f47fd623
Ayaka Hidaka <hidaka.ayaka105@gmail.com>
2023-01-15 21:39:56 +0900

    Modify interface & remove unnecessary intaface

--- functions/src/interface.ts
+++ functions/src/interface.ts
@@ -1,6 +1,6 @@
 interface TrainingPattern {
-  training: Array<Number>,
-  cardioHiit: Array<Number>
+  muscle: Array<Number>,
+  cardio: Array<Number>
 }
 
 interface Training {
@@ -11,10 +11,4 @@ interface Training {
   title: string,
   url: string,
   videoId: string,
-}
-
-interface FilterCondition {
-  channelId: Array<string>,
-  training: Array<string>,
-  cardioHiit: Array<string>
 }
