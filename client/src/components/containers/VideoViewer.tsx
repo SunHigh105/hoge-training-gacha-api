35d504dd5b9ecbccab6253d0ac846d68576df7cb
Ayaka Hidaka <hidaka.ayaka105@gmail.com>
2023-01-20 22:23:18 +0900

    Apply service in VideoViewer(WIP)

--- client/src/components/containers/VideoViewer.tsx
+++ client/src/components/containers/VideoViewer.tsx
@@ -1,8 +1,11 @@
 import React, {FC, useState} from "react";
 import { SemanticCOLORS } from "semantic-ui-react";
+import { minute } from "../../definitions/types";
+
+import { buildQueryParameter, getTrainigMenus } from "../../services/trainings";
 import { VideoViewer } from "../presentationals/parts/VideoViewer";
 
-export const VideoViewerContainer: FC<{minute: Number, color: SemanticCOLORS}> = ({
+export const VideoViewerContainer: FC<{minute: minute, color: SemanticCOLORS}> = ({
   minute,
   color
 }) => {
@@ -10,6 +13,11 @@ export const VideoViewerContainer: FC<{minute: Number, color: SemanticCOLORS}> =
 
   const handleViewer = () => {
     setIsDisplayed(!isDisplayed);
+
+    if (isDisplayed) {
+      const query = buildQueryParameter(minute);
+      const trainingMenu = getTrainigMenus(query);
+    }
   };
 
   return (
