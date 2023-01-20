7bd3912523d0f4868b416b991f169c2500ba4e4c
Ayaka Hidaka <hidaka.ayaka105@gmail.com>
2023-01-20 22:20:49 +0900

    Rename & move constants file

---
+++ client/src/definitions/constants.ts
@@ -0,0 +1,15 @@
+import { SemanticCOLORS } from "semantic-ui-react";
+import { minute } from "./types";
+
+export const MINUTES: Array<minute> = [10, 20, 30, 40, 50, 60];
+
+export const COLOR_PER_MINUTE: Array<SemanticCOLORS> = ['teal', 'green', 'olive', 'yellow', 'orange', 'red'];
+
+export const MINUTE_STRING = {
+  10: 'ten',
+  20: 'twenty',
+  30: 'thirty',
+  40: 'forty',
+  50: 'fifty',
+  60: 'sixty'
+};
