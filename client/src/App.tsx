9ecd355ff707a27c50f1a1502a26bfadd157b70f
Ayaka Hidaka <hidaka.ayaka105@gmail.com>
2023-01-25 22:20:52 +0900

    Remove unnecessary scripts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,6 @@ import React, { useState, useEffect, FC } from 'react';
 import { App } from './components/presentationals/App';
 
 export const AppContainer: FC =() => {
-  const [user, setUser] = useState({ user_id: '', name: '' });
-
-  // ログイン済みか判定
-  // 第二引数に[getUser]を設定すると何度もgetUserInfo()が実行されてしまう
-  useEffect(() => {
-    // getUserInfo();
-  }, []);
-  
   return (
     <App />
   );
