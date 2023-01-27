bc86bc8644286f2aa82022273cb3e3638f09ea47
Ayaka Hidaka <hidaka.ayaka105@gmail.com>
2023-01-27 21:49:41 +0900

    Add forty~sixty minute pattern

--- functions/src/constants.ts
+++ functions/src/constants.ts
@@ -67,10 +67,70 @@ const THIRTY_MIN_PATTERNS: Array<TrainingPattern> = [
   },
 ];
 
+const FORTY_MIN_PATTERNS: Array<TrainingPattern> = [
+  {
+    muscle: [5, 10],
+    cardio: [10, 15], 
+  },
+  {
+    muscle: [5, 5, 10],
+    cardio: [5, 15], 
+  },
+  {
+    muscle: [10, 10],
+    cardio: [10, 10],
+  },
+  {
+    muscle: [5, 5, 5],
+    cardio: [10, 15],
+  },
+];
+
+const FIFTY_MIN_PATTERNS: Array<TrainingPattern> = [
+  {
+    muscle: [5, 5, 10],
+    cardio: [10, 20], 
+  },
+  {
+    muscle: [10, 10],
+    cardio: [10, 20], 
+  },
+  {
+    muscle: [5, 5, 5],
+    cardio: [10, 10, 15],
+  },
+  {
+    muscle: [10, 15],
+    cardio: [10, 15],
+  },
+];
+
+const SIXTY_MIN_PATTERNS: Array<TrainingPattern> = [
+  {
+    muscle: [5, 5, 5, 10],
+    cardio: [15, 20], 
+  },
+  {
+    muscle: [10, 10, 10],
+    cardio: [10, 20], 
+  },
+  {
+    muscle: [5, 5, 5, 5],
+    cardio: [5, 10, 15],
+  },
+  {
+    muscle: [10, 15],
+    cardio: [15, 20],
+  },
+];
+
 const MINUTE_PATTERN = {
   ten: TEN_MIN_PATTERNS,
   twenty: TWENTY_MIN_PATTERNS,
-  thirty: THIRTY_MIN_PATTERNS
+  thirty: THIRTY_MIN_PATTERNS,
+  forty: FORTY_MIN_PATTERNS,
+  fifty: FIFTY_MIN_PATTERNS,
+  sixty: SIXTY_MIN_PATTERNS
 }
 
 module.exports = {
