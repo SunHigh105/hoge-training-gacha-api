77dd14d1b1f707ea6a43e4894902c2d6e0eaee79
Ayaka Hidaka <hidaka.ayaka105@gmail.com>
2023-02-01 23:37:24 +0900

    Modify loading

--- client/src/components/presentationals/parts/Loading.tsx
+++ client/src/components/presentationals/parts/Loading.tsx
@@ -1,12 +1,10 @@
-import React, {FC} from "react"
+import React from "react"
 import { Dimmer, Loader, Segment } from "semantic-ui-react"
 
 import '../../../css/Loading.css';
 
-export const Loading: FC<{}> = () => (
-  <Segment basic className='loading-wrapper'>
-    <Dimmer active inverted>
-      <Loader inverted>Loading...</Loader>
-    </Dimmer>
-  </Segment>
+export const Loading = () => (
+  <div className="loading-wrapper">
+    <Loader active inline>Loading...</Loader>
+  </div>
 );
