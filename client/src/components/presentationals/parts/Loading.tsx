190804aad3f83d3ac4caec88a379b1f77d02bc3b
Ayaka Hidaka <hidaka.ayaka105@gmail.com>
2023-02-02 21:54:50 +0900

    Add placeholder in Loading component

--- client/src/components/presentationals/parts/Loading.tsx
+++ client/src/components/presentationals/parts/Loading.tsx
@@ -1,10 +1,35 @@
 import React from "react"
-import { Dimmer, Loader, Segment } from "semantic-ui-react"
+import { Card, Grid, Loader, Placeholder } from "semantic-ui-react"
 
 import '../../../css/Loading.css';
 
 export const Loading = () => (
-  <div className="loading-wrapper">
-    <Loader active inline>Loading...</Loader>
+  <div className="loading-container">
+    <div className='loading-wrapper'>
+      <Loader active inline>Loading...</Loader>
+    </div>
+    <Card.Group>
+      {[1, 2, 3, 4, 5].map(num => (
+        <Card fluid key={num}>
+          <Card.Content>
+            <Grid>
+              <Grid.Row columns={2}>
+                <Grid.Column width={6}>
+                  <Placeholder className='loading-image-placeholder'>
+                    <Placeholder.Image />
+                  </Placeholder>
+                </Grid.Column>
+                <Grid.Column width={10}>
+                  <Placeholder>
+                    <Placeholder.Line />
+                    <Placeholder.Line />
+                  </Placeholder>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Card.Content>
+        </Card>
+      ))}
+    </Card.Group>
   </div>
 );
